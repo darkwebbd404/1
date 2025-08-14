694091f24980a041e7f170d6ff18ccac7d69246a
darkwebbd404 <darkwebbd404@gmail.com>
2025-08-14 11:18:39 +0600

    Add files via upload

---
+++ src/config/constants.py
@@ -0,0 +1,28 @@
+# Configuration constants
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# Global variables
+tempid = None
+sent_inv = False
+start_par = False
+pleaseaccept = False
+nameinv = "none"
+idinv = 0
+senthi = False
+statusinfo = False
+tempdata1 = None
+tempdata = None
+leaveee = False
+leaveee1 = False
+data22 = None
+isroom = False
+isroom2 = False
+
+# Game configuration
+paylod_token1 = "3a07312e3131312e32aa01026172b201203535656437353966636639346638353831336535376232656338343932663563ba010134ea0140366662376664656638363538666430333137346564353531653832623731623231646238313837666130363132633865616631623633616136383766316561659a060134a2060134ca03203734323862323533646566633136343031386336303461316562626665626466"
+freefire_version = "OB50"
+client_secret = "2ee44819e9b4598845141067b281621874d0d5d7af9d8f7e00c1e54715b7d1e3"
+chat_ip = "202.81.108.130"
+chat_port = 39699
